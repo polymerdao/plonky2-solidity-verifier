0969418827407b78050f5aceeebafc487e169444
Sai Deng <dengsai@hotmail.com>
2022-08-02 12:25:41 -0700

    Enable gas-reporter

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,9 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+    solidity: "0.8.9",
 };
 
 export default config;
+
+module.exports = {
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+            },
+        },
+    },
+    gasReporter: {
+        enabled: true
+    }
+}
