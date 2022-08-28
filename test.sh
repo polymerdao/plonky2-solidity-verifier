72a27bbfd634eaac2e8d05546e4e6f6f519e9c3a
Sai Deng <dengsai@hotmail.com>
2022-08-28 07:57:09 -0700

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-/Users/saideng/.cargo/bin/cargo test --color=always --package plonky2_solidity_verifier --lib verifier --no-fail-fast -- -Z unstable-options --show-output
+cargo test --color=always --package plonky2_solidity_verifier --lib verifier --no-fail-fast -- -Z unstable-options --show-output
 cd contract || exit
 npx hardhat compile
 npx hardhat test
-
